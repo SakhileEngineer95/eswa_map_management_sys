b34fefa01c95bb280aabde11f5abfd172515beaf
Sakhile Mahlalengangeni Mkhonta <135160725+SakhileEngineer95@users.noreply.github.com>
2026-06-24 09:49:47 +0200

    changed env variables

--- src/database/supabaseClient.js
+++ src/database/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_PUBLISHABLE_KEY;
+const supabaseUrl = "https://soepadocivmmyufnwrot.supabase.co" //import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = "sb_publishable_PQx4X9l1YaqU9kR_Eb770w_Wfe6NZ3W" //import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
